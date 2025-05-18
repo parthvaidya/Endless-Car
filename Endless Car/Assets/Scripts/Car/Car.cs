34356493c62ff22bd900b0a2bb06118cae456df9
Parth Vaidya <zeusparth99@gmail.com>
2025-05-18 16:54:16 +0530

    Updated Car
    Added camera control and object pool

--- Endless Car/Assets/Scripts/Car/Car.cs
+++ Endless Car/Assets/Scripts/Car/Car.cs
@@ -6,9 +6,13 @@ public class Car : MonoBehaviour
 {
     [SerializeField]
     Rigidbody rb;
+
+    [SerializeField] Transform gameModel;
     float accelerationMultiplier = 3;
     float brakeMultiplier = 15;
-    float steerMultiplier = 15;
+    float steerMultiplier = 5;
+    float MaxSteerVelocit = 2;
+    float MaxForwardVelocity = 30;
 
     Vector2 input = Vector2.zero;
     // Start is called before the first frame update
@@ -20,7 +24,7 @@ public class Car : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        gameModel.transform.rotation = Quaternion.Euler(0, rb.velocity.x * 5, 0);
     }
 
     private void FixedUpdate()
@@ -37,11 +41,17 @@ public class Car : MonoBehaviour
             Brake();
 
         Steer();
+
+        if (rb.velocity.z <= 0)
+            rb.velocity = Vector3.zero;
     }
 
     void Accelerate()
     {
         rb.drag = 0;
+        if (rb.velocity.z >= MaxForwardVelocity)
+            return;
+
         rb.AddForce(rb.transform.forward * accelerationMultiplier * input.y);
 
     }
@@ -60,7 +70,20 @@ public class Car : MonoBehaviour
     {
         if (Mathf.Abs(input.x) > 0)
         {
-            rb.AddForce(rb.transform.right * steerMultiplier * input.x);
+            float speedBaseSteerLimit = rb.velocity.z / 5.0f;
+            speedBaseSteerLimit = Mathf.Clamp01(speedBaseSteerLimit);
+
+            rb.AddForce(rb.transform.right * steerMultiplier * input.x * speedBaseSteerLimit);
+            float normalizedX = rb.velocity.x / MaxSteerVelocit;
+
+            normalizedX = Mathf.Clamp(normalizedX, -1.0f, 1.0f);
+
+            rb.velocity = new Vector3(normalizedX * MaxSteerVelocit, 0, rb.velocity.z);
+
+        }
+        else
+        {
+            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(0, 0, rb.velocity.z), Time.fixedDeltaTime * 3);
         }
     }
 
---
+++ Endless Car/Assets/Scripts/EndlessPool/EndlessPool.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessPool : MonoBehaviour
+{
+    [SerializeField] GameObject[] sectionPrefab;
+    GameObject[] sectionPool = new GameObject[20];
+    GameObject[] sections = new GameObject[20];
+    Transform playerCarTransform;
+    WaitForSeconds waitfor100ms = new WaitForSeconds(0.1f);
+
+    const float sectionLength = 26;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCarTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        int prefabIndex = 0;
+
+        for (int i = 0; i < sectionPool.Length; i++)
+        {
+            sectionPool[i] = Instantiate(sectionPrefab[prefabIndex]);
+            sectionPool[i].SetActive(false);
+
+            prefabIndex++;
+
+            if (prefabIndex > sectionPrefab.Length - 1)
+            {
+                prefabIndex = 0;
+            }
+        }
+
+        for (int i = 0; i < sections.Length; i++)
+        {
+            GameObject randomSection = GetRandomSectionFromPool();
+
+            randomSection.transform.position = new Vector3(sectionPool[i].transform.position.x, 0, i * sectionLength);
+            randomSection.SetActive(true);
+
+            sections[i] = randomSection;
+        }
+
+        StartCoroutine(UpdateLessOften());
+    }
+
+    IEnumerator UpdateLessOften()
+    {
+        while (true)
+        {
+            UpdatePosition();
+            yield return waitfor100ms;
+            
+        }
+    }
+
+    void UpdatePosition()
+    {
+        for (int i = 0; i < sections.Length; i++)
+        {
+            if (sections[i].transform.position.z - playerCarTransform.position.z < -sectionLength)
+            {
+                Vector3 lastSectionPosition = sections[i].transform.position;
+                sections[i].SetActive(false);
+
+                sections[i] = GetRandomSectionFromPool();
+
+                sections[i].transform.position = new Vector3(lastSectionPosition.x, 0, lastSectionPosition.z + sectionLength * sections.Length);
+                sections[i].SetActive(true);
+
+            }
+        }
+
+    
+    }
+
+    GameObject GetRandomSectionFromPool()
+    {
+        int randomIndex = Random.Range(0, sectionPool.Length);
+
+        bool isNewSectionFound = false;
+
+        while (!isNewSectionFound)
+        {
+            if (!sectionPool[randomIndex].activeInHierarchy)
+                isNewSectionFound = true;
+            else
+            {
+                randomIndex++;
+
+                if (randomIndex > sectionPool.Length - 1)
+                    randomIndex = 0;
+            }
+        }
+
+        return sectionPool[randomIndex];
+    }
+}
